75534356941a0581cd52bebc5342aea88cbdaef5
Dalerjon Ismatov <dalerjonismatov500@gmail.com>
2025-03-13 08:44:28 +0500

    ready vazifa2

--- src/App3.js
+++ src/App3.js
@@ -27,6 +27,7 @@ function App() {
     const filteredTasks = tasks.filter((task) => {
         if (filter === "active") return !task.completed;
         if (filter === "completed") return task.completed;
+        console.log("salom")
         return true;
     });
 
